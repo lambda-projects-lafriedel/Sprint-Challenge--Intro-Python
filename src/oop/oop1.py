9c17e75738f3157f3f3f31ecf54bf9f32c67b6b4
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-05-31 11:06:01 -0400

    Complete and test oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# This is the base/superclass
+class Vehicle:
+    pass
+
+# This is a subclass of Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# This is a subclass of Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# This is a subclass of GroundVehicle, which is a subclass of Vehicle
+class Car(GroundVehicle):
+    pass
+
+# This is a subclass of GroundVehicle, which is a subclass of Vehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+# This is a subclass of FlightVehicle, which is a subclass of Vehicle
+class Airplane(FlightVehicle):
+    pass
+
+# This is a subclass of FlightVehicle, which is a subclass of Vehicle
+class Starship(FlightVehicle):
+    pass
